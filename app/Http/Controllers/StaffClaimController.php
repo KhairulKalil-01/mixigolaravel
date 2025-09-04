3901840f1713a47aec5ecbe75606c022c8792144
khairulkalil <152203477+KhairulKalil-01@users.noreply.github.com>
2025-09-04 15:57:37 +0800

    staff claims and approval module

---
+++ app/Http/Controllers/StaffClaimController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StaffClaim;
+use Illuminate\Http\Request;
+
+class StaffClaimController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StaffClaim $staffClaim)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StaffClaim $staffClaim)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, StaffClaim $staffClaim)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StaffClaim $staffClaim)
+    {
+        //
+    }
+}
